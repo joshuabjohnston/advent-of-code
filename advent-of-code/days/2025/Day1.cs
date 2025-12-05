392a389ff07b4441e08c891a84192f729733cc3c
Joshua Johnston <jjohnston@CTI-APLT-001247.local>
2025-12-05 15:39:09 -0600

    Initial impl for Day1, star 2. Does not work.

--- advent-of-code/days/2025/Day1.cs
+++ advent-of-code/days/2025/Day1.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using org.jjohnston.aoc.days;
 
 namespace org.jjohnston.aoc.year2025;
@@ -14,11 +15,95 @@ public class Day1 : AbstractDay
 
         int numZeroes = 0;
 
+        foreach (String s in inputs)
+        {
+            char dir = s[0];
+            int num = int.Parse(s.Substring(1));
+
+            if (dir == 'R')
+            {
+                dial += num;
+            }
+            else if (dir == 'L')
+            {
+                dial -= num;
+            }
+
+            while (dial < 0 || dial >= 100)
+            {
+                if (dial < 0)
+                {
+                    dial += 100;
+                }
+                else if (dial >= 100)
+                {
+                    dial -= 100;
+                }
+            }
+
+            if (dial == 0)
+            {
+                numZeroes++;
+            }
+
+            if (debug)
+            {
+                Console.WriteLine($"{s}, Direction: {dir}, Number: {num} = {dial}. Current zeroes: {numZeroes}");
+            }
+        }
+
         return "number of zeroes == " + numZeroes;
     }
 
     public override string Star_2_Impl(string[] inputs, bool debug)
     {
-        throw new NotImplementedException();
+        int dial = 50;
+
+        int numZeroes = 0;
+
+
+        foreach (String s in inputs)
+        {
+            int startingDial = dial;
+
+            char dir = s[0];
+            int num = int.Parse(s.Substring(1));
+
+            while (num >= 100)
+            {
+                numZeroes++;
+                num -= 100;
+            }
+
+            if (dir == 'R')
+            {
+                dial += num;
+                if (dial >= 100)
+                {
+                    ++numZeroes;
+                    dial -= 100;
+                }
+            }
+            else if (dir == 'L')
+            {
+                dial -= num;
+                if (dial < 0)
+                {
+                    ++numZeroes;
+                    dial += 100;
+                }
+                else if (dial == 0)
+                {
+                    ++numZeroes;
+                }
+            }
+
+            if (debug)
+            {
+                Console.WriteLine($"{s}\t{startingDial} {(dir == 'L' ? "-" : "+")} {num} = {dial}\tNum zeroes == {numZeroes}");
+            }
+        }
+
+        return "number of zeros == " + numZeroes;
     }
 }
