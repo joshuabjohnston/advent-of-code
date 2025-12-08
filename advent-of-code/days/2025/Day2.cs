cba6d44fb20fca1eada57af669b5c01bd267ef23
Joshua Johnston <joshua.b.johnston@gmail.com>
2025-12-08 09:55:39 -0600

    Day 2, Star 2 placeholder code. Not impl.

--- advent-of-code/days/2025/Day2.cs
+++ advent-of-code/days/2025/Day2.cs
@@ -55,6 +55,38 @@ public class Day2 : AbstractDay
 
     public override string Star_2_Impl(string[] inputs, bool debug)
     {
-        throw new NotImplementedException();
+        String[] ranges = inputs[0].Split(",");
+
+        Int64 sumInvalidIds = 0;
+
+        foreach (string range in ranges)
+        {
+            String[] ends = range.Split("-");
+            Int64 low = Int64.Parse(ends[0]);
+            Int64 high = Int64.Parse(ends[1]);
+
+            Console.Out.WriteLine($"Range {low} - {high}");
+
+            for (Int64 i = low; i <= high; i++)
+            {
+                bool repeatPattern = isIdPatternThatRepeats(i);
+                if (repeatPattern)
+                {
+                    sumInvalidIds += i;
+                }
+
+                if (debug)
+                {
+                    Console.Out.WriteLine($"\t{i} -> {(repeatPattern ? "yes" : "no")}");
+                }
+            }
+        }
+
+        return "sum of invalid IDs = " + sumInvalidIds;
+    }
+
+    public bool isIdPatternThatRepeats(Int64 id)
+    {
+        return false;
     }
 }
